19972f13c6d1d4fd7bff8cb2a5b60bb009495493
wiliam-bermudez <wbermdezsols@byupathway.edu>
2026-03-22 15:44:47 -0600

    Error handling

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities/index")
 
 
 /* ***********************
@@ -33,12 +34,37 @@ app.use("/inv", inventoryRoute)
 //Index route
 app.get("/", baseController.buildHome)
 
+//Error handling 
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
+
+
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
 const port = process.env.PORT
 const host = process.env.HOST
+
+
+
 
 /* ***********************
  * Log statement to confirm server operation
